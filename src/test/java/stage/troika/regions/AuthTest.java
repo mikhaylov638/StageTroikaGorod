93585efcb894ce86c81e276c1fec7cee494e9c9f
mikhaylov638 <62191142+mikhaylov638@users.noreply.github.com>
2023-02-03 03:32:50 +0300

    Initial commit

---
+++ src/test/java/stage/troika/regions/AuthTest.java
@@ -0,0 +1,44 @@
+package stage.troika.regions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class AuthTest {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void auth() throws InterruptedException {
+        driver.get("https://stage-gorodtroika.rtgsoft.ru");
+        driver.manage().window().setSize(new Dimension(1536, 824));
+        driver.findElement(By.cssSelector(".MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.button-atom.Outlined.css-1ujsas3")).click();
+        driver.findElement(By.cssSelector(".sc-dvpmds")).click();
+        driver.findElement(By.cssSelector(".sc-dvpmds")).sendKeys("9645821798");
+        driver.findElement(By.cssSelector(".sc-dHIava")).click();//нажал далее
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector(".sc-dvpmds")).click();
+        driver.findElement(By.cssSelector(".sc-dvpmds")).sendKeys("0000");
+        driver.findElement(By.cssSelector(".sc-dHIava")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector(".sc-dvpmds")).click();
+        driver.findElement(By.cssSelector(".sc-dvpmds")).sendKeys("0000");
+        driver.findElement(By.cssSelector(".sc-dHIava")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector(".sc-jhaWeW")).click();
+        driver.findElement(By.cssSelector(".sc-jhaWeW")).sendKeys("0000");
+        driver.findElement(By.cssSelector(".sc-dHIava")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".sc-cLQEGU")).click();
+        Thread.sleep(1500);
+        driver.findElement(By.cssSelector(".sc-hMFtBS [href='/logout']")).click();
+    }
+}
+
---
+++ src/test/java/stage/troika/regions/TestNonAuthZone.java
@@ -0,0 +1,53 @@
+package stage.troika.regions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/*
+Тестирование неавторизованной зоны на регионы:
+1. Регион Москва (С тройкой):
+    FindText = 'Пополняйте транспортную карту «Тройка»,
+                приобретайте товары и услуги — получайте кешбэк на счет в Городе
+                и меняйте накопленные бонусы на скидки и подарки магазинов и сервисов.'
+2. Регион Самара (Без тройки):
+    FindText = 'Приобретайте товары и услуги, получайте кешбэк на счет в Городе,
+                меняйте накопленные бонусы на скидки и подарки магазинов и сервисов.'
+ */
+
+public class TestNonAuthZone {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void NonAuthWithTroika() throws InterruptedException{
+        driver.get("https://stage-gorodtroika.rtgsoft.ru");
+        driver.manage().window().setSize(new Dimension(1536, 824));
+        Thread.sleep(1500);
+        String defaultTextRegionWithTroika = "Пополняйте транспортную карту «Тройка», приобретайте товары и услуги — получайте кешбэк на счет в Городе и меняйте накопленные бонусы на скидки и подарки магазинов и сервисов.";
+        String subtitleText = driver.findElement(By.cssSelector(".description-block > .subtitle")).getText();
+        defaultTextRegionWithTroika.equals(subtitleText);
+    }
+
+    @Test
+    public void NonAuthWithOutTroika() throws InterruptedException{
+        driver.get("https://stage-gorodtroika.rtgsoft.ru");
+        driver.manage().window().setSize(new Dimension(1536, 824));
+        Thread.sleep(1500);
+        driver.findElement(By.cssSelector(".geo-btn-desktop")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector(".geo-btn-desktop")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("[data-place='5']")).click();
+        String subtitleText = driver.findElement(By.cssSelector(".description-block > .subtitle")).getText();
+        String defaultTextRegionWithOutTroika = "Приобретайте товары и услуги, получайте кешбэк на счет в Городе, меняйте накопленные бонусы на скидки и подарки магазинов и сервисов.";
+        defaultTextRegionWithOutTroika.equals(subtitleText);
+    }
+}

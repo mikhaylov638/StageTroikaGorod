0a2d57842bae60a33cc80d14f0cc008a3f2cbdd0
mikhaylov638 <62191142+mikhaylov638@users.noreply.github.com>
2023-02-06 16:16:38 +0300

    Добавил chromedriver в auth test

--- src/test/java/stage/troika/regions/AuthTest.java
+++ src/test/java/stage/troika/regions/AuthTest.java
@@ -8,28 +8,33 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AuthTest {
+    WebDriver driver;
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "StageTroikaGorod\\src\\resources\\drivers\\chromedriver.exe");
     }
 
-    @Test
-    public void auth() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+    @BeforeMethod
+    public void beforeTest(){
+        driver = new ChromeDriver();
         driver.get("https://stage-gorodtroika.rtgsoft.ru");
-        driver.manage().window().setSize(new Dimension(1536, 824));
+        driver.manage().window().maximize();
         driver.findElement(By.cssSelector(".MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textPrimary.MuiButton-sizeMedium.MuiButton-textSizeMedium.button-atom.Outlined.css-1ujsas3")).click();
         driver.findElement(By.cssSelector(".sc-dvpmds")).click();
         driver.findElement(By.cssSelector(".sc-dvpmds")).sendKeys("9645821798");
         driver.findElement(By.cssSelector(".sc-dHIava")).click();//нажал далее
-        Thread.sleep(3000);
+    }
+
+    @Test
+    public void auth() throws InterruptedException {
+        Thread.sleep(1500);
         driver.findElement(By.cssSelector(".sc-dvpmds")).click();
         driver.findElement(By.cssSelector(".sc-dvpmds")).sendKeys("0000");
         driver.findElement(By.cssSelector(".sc-dHIava")).click();
-        Thread.sleep(3000);
+        Thread.sleep(1500);
         driver.findElement(By.cssSelector(".sc-dvpmds")).click();
         driver.findElement(By.cssSelector(".sc-dvpmds")).sendKeys("0000");
         driver.findElement(By.cssSelector(".sc-dHIava")).click();
-        Thread.sleep(3000);
+        Thread.sleep(1500);
         driver.findElement(By.cssSelector(".sc-jhaWeW")).click();
         driver.findElement(By.cssSelector(".sc-jhaWeW")).sendKeys("0000");
         driver.findElement(By.cssSelector(".sc-dHIava")).click();
